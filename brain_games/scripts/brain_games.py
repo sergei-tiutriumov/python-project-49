a609b278c32112fbc546343435045f884a4f42de
sergei.tiutriumov <sergei.tiutriumov@gorfax.ru>
2024-06-12 16:32:18 +0300

    welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+from .cli import welcome_user
 def main():
-    a = "poetry run python -m brain_games.scripts.brain_games\nWelcome to the Brain Games!"
+    a = "Welcome to the Brain Games!"
     print(a)
+    welcome_user()
+#    name = prompt.string('May I have your name? ')
+#    print(f'Hello, {name}')
 
 if __name__ == '__main__':
     main()
+
